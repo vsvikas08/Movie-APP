90453bf69ac254475c3d5f2e073a5a15a4f38f37
Your Name <vikas3517@gmail.com>
2021-08-30 22:57:24 +0530

    Add action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,13 @@
 // {
 // 	type: 'DECREASE_COUNT'
 // }
+
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// Action creators
+export function addMovies(movies){
+	return {
+		type: ADD_MOVIES,
+		movies
+	}
+}

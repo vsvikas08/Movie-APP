78b65710523dcfb99a19ab02869a4f25c7ce9d7d
Your Name <vikas3517@gmail.com>
2021-09-05 17:10:48 +0530

    Changing the state

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
         console.log('State ',this.props.store.getState());
     }
     render(){
-        const movies = this.props.store.getState();
+        const {list} = this.props.store.getState();
         return (
         <div className="App">
             <Navbar />
@@ -28,7 +28,7 @@ class App extends React.Component {
                 </div>
 
                 <div className="list">
-                    {movies.map((movie,index) => (
+                    {list.map((movie,index) => (
                         <MovieCard movie={movie} key={`movies-${index}`} />
                     ))}
                 </div>
